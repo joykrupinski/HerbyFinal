ef6c42a2a7700a7887687319ed0c960f31cfdf74
joykrupinski <84975460+joykrupinski@users.noreply.github.com>
2021-07-16 17:00:17 +0200

    Final Comments to herby.py

--- herby.py
+++ herby.py
@@ -4,100 +4,95 @@ from upm import pyupm_buzzer as upmBuzzer
 import pandas as pd
 from datetime import date
 
-#Moisture and buzzer
+#Moisture import
 from grove.grove_moisture_sensor import GroveMoistureSensor
+
+#display import
 from grove.display.jhd1802 import JHD1802
 
-#TDS
+#TDS import
 from TDS import GroveTDS
 
-#Light sensor
+#Light import
 from grove.grove_light_sensor_v1_2 import GroveLightSensor
 
-#humidity and temperature
+#humidity and temperature import
 from seeed_dht import DHT
 
-#import fuer csv Datei
+#import for csv file
 import csv
 
-#main function
-        
-
-def moisture_tds_main():
-
-        lcd = JHD1802()
-        
+#seperate sensor functions to read and process values, to be called in main
+  
+#tds and moisture       
+def moisture_tds_main():    
+        #read sensor values
         sensor = GroveMoistureSensor(4)
-        
-        #buzzer = upmBuzzer.Buzzer(getGpioLookup('GPIO12'))
         sensor_tds = GroveTDS(0)
+        
+        #print sensor values
         print('TDS Value: {0}'.format(sensor_tds.TDS))
         
         mois = sensor.moisture
         if 0 <= mois and mois < 300:
             level = 'dry'
-            #buzzer.playSound(upmBuzzer.BUZZER_DO, 200000)
         elif 300 <= mois and mois < 600:
             level = 'moist'
         else:
             level = 'wet'
         
-                
-        print('moisture: {}, {}'.format(mois, level))
-            
-        lcd.setCursor(0, 0)
-        lcd.write('moisture: {0:>6}'.format(level))
-            
-        lcd.setCursor(1,0)
-        lcd.write('TDS: {0:>12}'.format(sensor_tds.TDS)) #hier muss noch ein threshold hin
-        
+        #print values in terminal (level printed in terminal)      
+        print('moisture: {}, {}'.format(mois, level)) 
         return mois,sensor_tds.TDS
         
-        
-def Light_main():           
+#light       
+def Light_main():      
+        #read sensor values
         sensor = GroveLightSensor(2)
+        #print values in terminal
         print('light value {}'.format(sensor.light))
         
         return sensor.light
         
-
+#temperature and humidity
 def temp_hum_main():
+        #display
         lcd = JHD1802()
-        
+        #find correct port in base hat
         sensor = DHT('11', 5)
         
-
+        #read sensor values
         humi, temp = sensor.read()
+        #print values in terminal
         print('temperature {}C, humidity {}%'.format(temp,humi))
-                
+            
+        #print values on display
         lcd.setCursor(0,0)
         lcd.write('temperature: {0:2}C'.format(temp))
-
         lcd.setCursor(1,0)
         lcd.write('humidity: {0:5}%'.format(humi))
         
         return humi,temp
         
-
+#main functions calls all sensor functions and writes values in csv file 
 def main():
     
     while True:
         
-
+        #call all sensors
         mois,tds = moisture_tds_main()
-        time.sleep(2)
         light = Light_main()
         hum,temp = temp_hum_main()
         
-    
-        #today = pd.to_datetime('today')
+        #get current date in correct format and write all sensor values in file with according date
         currentDate = date.today()
         today = currentDate.strftime('%m/%d/%y')
         toWrite = [tds, mois, light, temp, hum]
-        
+        #create data frame 
         df = pd.read_csv('test.csv')
         df[today] = toWrite
         df.to_csv('test.csv', index=False)
+        #sleep for 24h, programm runs once a day
         time.sleep(86400)
         
 
